64dca74081b25c82441a90a81650e428b3377e9f
Amit Kaplan <amit.y.kaplan@gmail.com>
2023-12-31 20:56:53 +0200

    test 16 - Q1+2

--- src/EX17/Q2.java
+++ src/EX17/Q2.java
@@ -1,5 +1,46 @@
-package test;
+package EX17;
 
-public class Q2{
-	
+public class Q2 extends Thread {
+    Runnable task;
+
+    volatile boolean busy;
+    public synchronized boolean setTask(Runnable runnable) {
+//        if(this.task!=null)
+//            return false;
+//        else{
+//            this.task = runnable;
+//            notify();
+//            return busy= true;
+//        }
+        if(this.task==null) {
+            this.task=runnable;
+            notify();
+            return true;
+        }
+        return false;
+    }
+    public void run(){
+        while (!busy){
+            if (task!=null)
+                task.run();
+            synchronized (this){
+                if(!busy)
+                {
+                    task =null;
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {}
+
+                }
+            }
+        }
+    }
+
+    public synchronized void close() {
+        busy =true;
+        notify();
+    }
+
+
 }
+
